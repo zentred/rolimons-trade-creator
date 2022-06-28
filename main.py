b6b779da223da6c7b96e6fd9243e2b65e4472a1e
zentred <87871486+zentred@users.noreply.github.com>
2022-06-28 21:50:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import requests, time, configparser, json, threading, ctypes, random, colorama, os
+import requests, time, configparser, json, threading, ctypes, random, colorama, os, re
 from colorama import init, Fore
 from threading import Thread
+from requests_html import HTMLSession
+session = HTMLSession()
 init()
 
 sent = 0
@@ -17,7 +19,18 @@ class Player:
         self.currSend = []
         Thread(target=self.overall).start()
 
+    def updateRolimons(self):
+        while True:
+            try:
+                session.get(f'https://www.rolimons.com/api/playerassets/{self.userId}').text # htmlSession request which executes page javascript which updates inventory so no more failed
+                return None
+            except:
+                time.sleep(5)
+                pass
+
     def highest(self):
+        self.updateRolimons()
+        time.sleep(15)
         totalLimiteds = requests.get(f'https://inventory.roblox.com/v1/users/{self.userId}/assets/collectibles?sortOrder=Asc&limit=100&cursor=').json()['data']
         self.myAssets = [limited['assetId'] for limited in totalLimiteds]
         random.shuffle(self.myAssets)
@@ -31,25 +44,40 @@ class Player:
 
     def send(self):
         global sent, failed
-        with lock: print(f'{Fore.WHITE}[{Fore.LIGHTBLUE_EX}SENDING{Fore.WHITE}] {self.userId} is about to send {str(self.currSend)}')
-        json = {"player_id":self.userId,"offer_item_ids":self.currSend,"request_item_ids":[],"request_tags":["any","demand","upgrade","rap"]}
-        r = requests.post('https://www.rolimons.com/tradeapi/create', json=json, cookies={'_RoliVerification': self.roliVerification, '_RoliData': self.roliData})
+
+        r = requests.post(
+            'https://www.rolimons.com/tradeapi/create',
+
+            cookies = {
+                '_RoliVerification': self.roliVerification,
+                '_RoliData': self.roliData
+                },
+
+            json = {
+                "player_id":self.userId,
+                "offer_item_ids":self.currSend,
+                "request_item_ids":[],
+                "request_tags":["any","demand","upgrade","rap"]
+                }
+            )
+
         if r.json()['success'] == True:
-            with lock: print(f'{Fore.WHITE}[{Fore.GREEN}CREATED{Fore.WHITE}] {self.userId} created a trade ad')
+            with lock: print(f'{Fore.LIGHTBLACK_EX}({Fore.LIGHTCYAN_EX}{self.userId}{Fore.LIGHTBLACK_EX}){Fore.WHITE} Created trade ad > {str(self.currSend)}')
             sent += 1
         else:
-            with lock: print(f'{Fore.WHITE}[{Fore.GREEN}FAILED{Fore.WHITE}] {self.userId} was unable to create a trade ad')
+            with lock: print(f'{Fore.LIGHTBLACK_EX}({Fore.LIGHTCYAN_EX}{self.userId}{Fore.LIGHTBLACK_EX}){Fore.WHITE} Unable to create trade ad')
             failed += 1
 
 
     def overall(self):
         while True:
             self.highest()
-            time.sleep(905)
+            time.sleep(900)
 
 def title():
     while True:
         ctypes.windll.kernel32.SetConsoleTitleW(f'Sent: {sent} | Failed: {failed}')
+        time.sleep(1)
 
 Thread(target=title).start()
 
